a4c6e4fae0097c13012e289da7a397d61362657c
Jose David Lagos Morales <consultor_dgpmacdf32@mef.gob.pe>
2024-10-05 10:26:13 -0500

    comentarios

--- src/main/java/pe/com/cayetano/see/discoveryservice/DiscoveryServiceApplication.java
+++ src/main/java/pe/com/cayetano/see/discoveryservice/DiscoveryServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/** clase principal de la clase discovery
+ *
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServiceApplication {
